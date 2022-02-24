7baa3e2fea7e0bdf8569dcc1c1ed339b951ac5fc
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-02-23 21:02:32 -0800

    added variable and functions for Live Currency Rates class.

--- app/src/main/java/com/example/mapapplication/LiveCurrencyRates.kt
+++ app/src/main/java/com/example/mapapplication/LiveCurrencyRates.kt
@@ -1,5 +1,16 @@
 package com.example.mapapplication
 
-class LiveCurrencyRates {
+import org.json.JSONObject
 
+class LiveCurrencyRates() {
+    // store json data fetched from api in this variable
+    private var rateData: JSONObject? = null
+
+    fun updateRates (data: JSONObject) {
+        rateData = data
+    }
+
+    fun recentRates (){
+        // return json object of recent updated rates
+    }
 }
