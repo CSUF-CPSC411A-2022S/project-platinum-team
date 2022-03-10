e3856c6ac0072f8dbd912a186521b5d657dc2295
ArqumAhmed <arqumahmed@csu.fullerton.edu>
2022-03-09 16:29:06 -0800

    Created functional graph class

--- app/src/main/java/com/example/mapapplication/MainActivity.kt
+++ app/src/main/java/com/example/mapapplication/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.mapapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.data2viz.charts.chart.Chart
+import io.data2viz.charts.chart.chart
+import io.data2viz.charts.chart.discrete
+import io.data2viz.charts.chart.mark.area
+import io.data2viz.charts.chart.quantitative
+import io.data2viz.geom.Size
+import io.data2viz.viz.VizContainerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(Graph(this))
     }
 }
+
