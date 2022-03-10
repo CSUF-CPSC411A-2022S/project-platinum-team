d72a0a3a6ec7c56d644680dda35e04b42d2434b7
ArqumAhmed <arqumahmed@csu.fullerton.edu>
2022-03-09 20:55:13 -0800

    Added Graph class fragment

---
+++ app/src/main/java/com/example/mapapplication/ModGraph.kt
@@ -0,0 +1,29 @@
+package com.example.mapapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ModGraph.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ModGraph : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.modgraph, container, false)
+    }
+
+
+}
