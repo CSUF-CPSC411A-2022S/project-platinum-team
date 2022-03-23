41a617e7868ff2ed1c7c7b3bee869e58889b0b62
josearceiv <josearce@csu.fullerton.edu>
2022-03-22 18:43:45 -0700

    added to clear and enter buttons with the RecyclerView List

--- app/src/main/java/com/example/mapapplication/ConversionHistory.kt
+++ app/src/main/java/com/example/mapapplication/ConversionHistory.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.currencyapplication.R
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
