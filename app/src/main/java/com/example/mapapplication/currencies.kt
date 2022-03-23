f1952c2cea121514d1726c7fcb3753ab09f33db0
Supernova284 <60196464+Supernova284@users.noreply.github.com>
2022-03-22 19:02:39 -0700

    Added another DropDown button and implemented Currencies.xml into navigation.xml. Also created a home fragment

--- app/src/main/java/com/example/mapapplication/currencies.kt
+++ app/src/main/java/com/example/mapapplication/currencies.kt
@@ -6,7 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import com.example.mapapplication.databinding.FragmentCurrenciesBinding
+import com.example.currencyapplication.R
+import com.example.currencyapplication.databinding.FragmentCurrenciesBinding
 
 class Currencies : Fragment() {
 
