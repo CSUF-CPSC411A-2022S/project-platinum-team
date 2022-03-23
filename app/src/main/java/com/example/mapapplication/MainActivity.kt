3608d2b0fd1a0fa51a0ab656fa3d8ed52b162fdd
ArqumAhmed <arqumahmed@csu.fullerton.edu>
2022-03-22 18:05:41 -0700

    Added Graph class fragment

--- app/src/main/java/com/example/mapapplication/MainActivity.kt
+++ app/src/main/java/com/example/mapapplication/MainActivity.kt
@@ -5,11 +5,20 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.data2viz.charts.chart.Chart
 import io.data2viz.charts.chart.chart
-import io.data2viz.charts.chart.discrete
-import io.data2viz.charts.chart.mark.area
 import io.data2viz.charts.chart.quantitative
 import io.data2viz.geom.Size
-import io.data2viz.viz.VizContainerView
+import io.data2viz.charts.*
+import io.data2viz.charts.dimension.*
+import io.data2viz.charts.chart.*
+import io.data2viz.charts.chart.mark.*
+import io.data2viz.charts.viz.*
+import io.data2viz.charts.layout.*
+import io.data2viz.charts.core.*
+import io.data2viz.geom.*
+import io.data2viz.color.*
+import io.data2viz.color.EncodedColors.Companion.set1
+import io.data2viz.color.EncodedColors.Companion.set2
+import io.data2viz.math.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
