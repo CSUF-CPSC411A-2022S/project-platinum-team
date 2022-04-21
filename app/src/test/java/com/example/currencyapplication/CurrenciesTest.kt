47fb7f494e22e89e6af3adbfde7552df31909f6f
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-04-21 13:00:21 -0700

    Created test cases for CurrenciesTest.kt fragment

---
+++ app/src/test/java/com/example/currencyapplication/CurrenciesTest.kt
@@ -0,0 +1,15 @@
+package com.example.currencyapplication
+
+import junit.framework.TestCase
+import org.junit.Test
+
+class CurrenciesTest : TestCase() {
+    @Test
+    fun testOnCreate() {}
+
+    @Test
+    fun testOnCreateView() {}
+
+    @Test
+    fun testNewInstance() {}
+}
