651f5269aae6ddb2ff6dfd789c4e2f89352e4b45
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-05-11 21:54:17 -0700

    Updated currencies fragment to use api

--- app/src/main/java/com/example/currencyapplication/Currencies.kt
+++ app/src/main/java/com/example/currencyapplication/Currencies.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
+import com.example.currencyapplication.conversion.ConversionViewModel
 import com.example.currencyapplication.databinding.FragmentCurrenciesBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,16 +19,18 @@ import com.example.currencyapplication.databinding.FragmentCurrenciesBinding
  * create an instance of this fragment.
  */
 class Currencies : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentCurrenciesBinding.inflate(layoutInflater)
+        val binding: FragmentCurrenciesBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_currencies, container, false)
         val Currencies = resources.getStringArray(R.array.Currencies)
+        val model = ConversionViewModel()
         val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, Currencies)
         binding.currencyFrom.setAdapter(arrayAdapter)
         binding.currencyTo.setAdapter(arrayAdapter)
+        binding.conversionViewModel = model
+        binding.lifecycleOwner = this
 
         return binding.root
     }
